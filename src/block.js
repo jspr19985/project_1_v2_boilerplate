652a08f89b101fc8794f7a518c6b994b4ec7edf7
jspr <joannasprott@hotmail.com>
2023-02-18 18:07:09 +0000

    working version

--- src/block.js
+++ src/block.js
@@ -15,37 +15,39 @@ const hex2ascii = require('hex2ascii');
 class Block {
 
     // Constructor - argument data will be the object containing the transaction data
-	constructor(data){
-		this.hash = null;                                           // Hash of the block
-		this.height = 0;                                            // Block Height (consecutive number of each block)
-		this.body = Buffer.from(JSON.stringify(data)).toString('hex');   // Will contain the transactions stored in the block, by default it will encode the data
-		this.time = 0;                                              // Timestamp for the Block creation
-		this.previousBlockHash = null;                              // Reference to the previous Block Hash
+    constructor(data) {
+        this.hash = null;                                           // Hash of the block
+        this.height = 0;                                            // Block Height (consecutive number of each block)
+        this.body = Buffer.from(JSON.stringify(data)).toString('hex');   // Will contain the transactions stored in the block, by default it will encode the data
+        this.time = 0;                                              // Timestamp for the Block creation
+        this.previousBlockHash = null;                              // Reference to the previous Block Hash
     }
-    
+
     /**
      *  validate() method will validate if the block has been tampered or not.
      *  Been tampered means that someone from outside the application tried to change
      *  values in the block data as a consecuence the hash of the block should be different.
      *  Steps:
-     *  1. Return a new promise to allow the method be called asynchronous.
-     *  2. Save the in auxiliary variable the current hash of the block (`this` represent the block object)
-     *  3. Recalculate the hash of the entire block (Use SHA256 from crypto-js library)
-     *  4. Compare if the auxiliary hash value is different from the calculated one.
-     *  5. Resolve true or false depending if it is valid or not.
+     *  1. Return a new promise to allow the method be called asynchronous. ✓
+     *  2. Save the in auxiliary variable the current hash of the block (`this` represent the block object) ✓
+     *  3. Recalculate the hash of the entire block (Use SHA256 from crypto-js library) ✓
+     *  4. Compare if the auxiliary hash value is different from the calculated one. ✓
+     *  5. Resolve true or false depending if it is valid or not. ✓
      *  Note: to access the class values inside a Promise code you need to create an auxiliary value `let self = this;`
      */
     validate() {
         let self = this;
         return new Promise((resolve, reject) => {
             // Save in auxiliary variable the current block hash
-                                            
+            let hash = self.hash;
+
             // Recalculate the hash of the Block
+            let newHash = SHA256(JSON.stringify(self)).toString();
+
             // Comparing if the hashes changed
             // Returning the Block is not valid
-            
             // Returning the Block is valid
-
+            resolve(hash === newHash);
         });
     }
 
@@ -59,11 +61,19 @@ class Block {
      *     or Reject with an error.
      */
     getBData() {
-        // Getting the encoded data saved in the Block
+        let self = this;
+        // Getting the encoded data saved in the Block               
         // Decoding the data to retrieve the JSON representation of the object
-        // Parse the data to an object to be retrieve.
-
+        const decoded = hex2ascii(this.body);
         // Resolve with the data if the object isn't the Genesis block
+        return new Promise((resolve, reject) => {
+            if (self.height !== 0) {
+                // Parse the data to an object to be retrieve.
+                resolove(JSON.parse(decoded));
+            } else {
+                reject(Error("Object is genesis block."));
+            }
+        });
 
     }
 
